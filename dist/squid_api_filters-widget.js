7a62b14083da3316fe24e30a8e461e28120e3ecb
Thomas Scott <thomas.scott@squidsolutions.com>
2015-03-05 15:59:57 +0100

    Disable filter button when status model is running

--- dist/squid_api_filters-widget.js
+++ dist/squid_api_filters-widget.js
@@ -572,105 +572,6 @@ function program4(depth0,data) {
 }));
 
 (function (root, factory) {
-    root.squid_api.view.CategoricalInternalSelectedView = factory(root.Backbone, root.squid_api);
-}(this, function (Backbone, squid_api) {
-
-    var View = Backbone.View.extend({
-
-        model : null,
-        filterStore : null,
-        format : null,
-
-        initialize : function(options) {
-            if (!this.model) {
-                this.model = squid_api.model.filters;
-            }
-            if (options.filterStore) {
-                this.filterStore = options.filterStore;
-            }
-
-            this.filterPanelTemplate = squid_api.template.squid_api_filters_categorical_selected_view;
-
-            if (options.format) {
-                this.format = options.format;
-            } else {
-                if (d3) {
-                    this.format = d3.time.format("%Y-%m-%d");
-                } else {
-                    this.format = function(val){return val;};
-                }
-            }
-
-            this.model.on("change", this.render, this);
-        },
-
-        events: {
-            "click .facet-remove": function(event) {
-                // Obtain facet name / value
-                var facetName = $(event.currentTarget).parent("li").attr("attr-name");
-                var facetId = $(event.currentTarget).parent("li").attr("attr-id");
-
-                var selection = this.model.get("selection");
-                if (selection) {
-                    if (selection.facets) {
-                        var facets = selection.facets;
-                        var updatedFacets = {facets:[]};
-                        for (i=0; i<facets.length; i++) {
-                            var selectedItems = facets[i].selectedItems;
-                            if (selectedItems.length > 0) {
-                                var arr = [];
-                                for (ix=0; ix<selectedItems.length; ix++) {
-                                    if (selectedItems[ix].id) {
-                                        if (facetId !== selectedItems[ix].id) {
-                                            arr.push(selectedItems[ix]);
-                                        }
-                                    }
-                                }
-                                facets[i].selectedItems = arr;
-                                updatedFacets.facets.push(facets[i]);
-                            } else {
-                                updatedFacets.facets.push(facets[i]);
-                            }
-                        }
-                        this.model.set("selection", updatedFacets);
-                        this.model.trigger("change");             
-                    }
-                }
-            }
-        },
-
-        render : function() {
-            var selection = this.model.get("selection");
-            var selFacets = [];
-            var noData = true;
-            var noDataMessage = "No Filters Selected";
-            if (selection) {
-                 if (selection.facets) {
-                    var facets = selection.facets;
-                    for (i=0; i<facets.length; i++) {
-                        var selectedItems = facets[i].selectedItems;
-                            if (facets[i].dimension.type !== "CONTINUOUS") {
-                                for (ix=0; ix<selectedItems.length; ix++) {
-                                    noData = false;
-                                    var obj = {};
-                                    obj.facetItem = selectedItems[ix].value;
-                                    obj.facetItemId = selectedItems[ix].id;
-                                    obj.facetName = facets[i].dimension.name;
-                                    obj.facetNameId = facets[i].id;
-                                    selFacets.push(obj);
-                                }
-                            }
-                        }
-                    }
-                }
-                this.$el.html(this.filterPanelTemplate({facets: selFacets, noData: noData, noDataMessage: noDataMessage}));
-            }
-        });
-
-    return View;
-}));
-
-(function (root, factory) {
     root.squid_api.view.CategoricalPagingView = factory(root.Backbone, root.squid_api);
 }(this, function (Backbone, squid_api) {
 
@@ -1034,7 +935,22 @@ function program4(depth0,data) {
             }, this);
 
             this.setCurrentModel();
+
+            // listen for global status change
+            squid_api.model.status.on('change:status', this.statusUpdate, this);
+
             this.render();
+        },
+
+        statusUpdate : function() {
+            var running = (squid_api.model.status.get("status") != squid_api.model.status.STATUS_DONE);
+            if (running) {
+                // computation is running : disable input
+                this.$el.find("button").attr("disabled","disabled");
+            } else {
+                // computation is done : enable input
+                this.$el.find("button").removeAttr("disabled");
+            }
         },
         
         setCurrentModel : function() {

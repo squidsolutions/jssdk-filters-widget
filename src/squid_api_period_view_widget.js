301dea0e8580bf0879b674a3c27aa006c533c97a
obalbous <oba@squid>
2015-03-16 18:09:17 +0100

    fixed case when selection updated to selection without a timeFacet

--- src/squid_api_period_view_widget.js
+++ src/squid_api_period_view_widget.js
@@ -34,33 +34,40 @@
             var facets;
             if (sel && sel.facets) {
                 facets = sel.facets;
+                var timeFacet;
                 for (var i = 0; i < facets.length; i++) {
                     var facet = facets[i];
                     if (facet.dimension.type == "CONTINUOUS") {
-                        var items = facet.items;
-                        var selItems = facet.selectedItems;
-                        var name = facet.dimension.name;
-                        var facetId = facet.id;
-                        var startDateStr;
-                        var endDateStr;
-                        if (items && items.length > 0) {
-                            if (selItems && selItems.length > 0) {
-                                // get selected values instead
-                                startDateStr = selItems[0].lowerBound;
-                                endDateStr = selItems[0].upperBound;
-                            } else {
-                                startDateStr = items[0].lowerBound;
-                                endDateStr = items[0].upperBound;
-                            }
-                            // apply formatting
-                            if (this.format) {
-                                endDateStr = moment.utc(endDateStr).format("MMM Do YYYY");
-                                startDateStr = moment.utc(startDateStr).format("MMM Do YYYY");
-                            }
-                        }
-                        this.$el.find("#sq-startDate").text(startDateStr);
-                        this.$el.find("#sq-endDate").text(endDateStr);
+                        timeFacet = facet;
                     }
+                }
+                if (timeFacet) {
+                    var items = timeFacet.items;
+                    var selItems = timeFacet.selectedItems;
+                    var name = timeFacet.dimension.name;
+                    var facetId = timeFacet.id;
+                    var startDateStr;
+                    var endDateStr;
+                    if (items && items.length > 0) {
+                        if (selItems && selItems.length > 0) {
+                            // get selected values instead
+                            startDateStr = selItems[0].lowerBound;
+                            endDateStr = selItems[0].upperBound;
+                        } else {
+                            startDateStr = items[0].lowerBound;
+                            endDateStr = items[0].upperBound;
+                        }
+                        // apply formatting
+                        if (this.format) {
+                            endDateStr = moment.utc(endDateStr).format("MMM Do YYYY");
+                            startDateStr = moment.utc(startDateStr).format("MMM Do YYYY");
+                        }
+                    }
+                    this.$el.find("#sq-startDate").text(startDateStr);
+                    this.$el.find("#sq-endDate").text(endDateStr);
+                } else {
+                    this.$el.find("#sq-startDate").text("");
+                    this.$el.find("#sq-endDate").text("");
                 }
             }
         }

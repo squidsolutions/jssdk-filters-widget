e4bb958798294a4437b3e634ef87b056be287af4
Thomas Scott <thomas.scott@squidsolutions.com>
2015-05-29 15:18:19 +0200

    Ui-Dialog extension tidy

--- src/squid_api_dialog_extension.js
+++ src/squid_api_dialog_extension.js
@@ -1,35 +1,31 @@
 $.widget( "ui.dialog", $.ui.dialog, {
-  options: {
-    clickOutside: false, // Determine if clicking outside the dialog shall close it
-    clickOutsideTrigger: "", // Element (id or class) that triggers the dialog opening
-    facetResultsWrapper: ".squid-api-categorical-facet-view" // Set facet view wrapper to prevent close from internal facet view
-  },
-
-  open: function() {
-    var clickOutsideTriggerEl = $( this.options.clickOutsideTrigger );
-    var that = this;
-    var facetResultsWrapper = $( this.options.facetResultsWrapper );
-    
-    if (this.options.clickOutside){
-      // Add document wide click handler for the current dialog namespace
-      $(document).on( "click.ui.dialogClickOutside" + that.eventNamespace, function(event){
-        // Provide an exception rule with .squid-api-action to not 
-        if ( $(event.target).closest($(clickOutsideTriggerEl)).length === 0 && $(event.target).closest($(that.uiDialog)).length === 0 && $(event.target).parents(".squid-api-action").length === 0){
-          that.close();
+    options: {
+        clickOutside: false, // Determine if clicking outside the dialog shall close it
+        clickOutsideTrigger: "", // Element (id or class) that triggers the dialog opening
+        parentContains: ".squid-api-action",
+    },
+    open: function() {
+        var clickOutsideTriggerEl = $( this.options.clickOutsideTrigger );
+        var parentContains = this.options.parentContains;
+        var me = this;
+        if (this.options.clickOutside) {
+            // Add document wide click handler for the current dialog namespace
+            $(document).on( "click.ui.dialogClickOutside" + me.eventNamespace, function(event) {
+                var item1 = $(event.target).closest($(clickOutsideTriggerEl));
+                var item2 = $(event.target).closest($(me.uiDialog));
+                var item3 = $(event.target).parents(parentContains);
+                if ((item1.length === 0 && item2.length === 0 && item3.length === 0)) {
+                    me.close();
+                }
+            });
         }
-      });
-    }
-    
-    this._super(); // Invoke parent open method
-  },
-  
-  close: function() {
-    var that = this;
-    
-    // Remove global click handler for current dialog
-    $(document).off( "click.ui.dialogClickOutside" + that.eventNamespace );
-    
-    this._super(); // Invoke parent's close method
-  },  
+        this._super(); // Invoke parent open method
+    },
+    close: function() {
+        var me = this;
 
+        // Remove global click handler for current dialog
+        $(document).off( "click.ui.dialogClickOutside" + me.eventNamespace );
+        this._super(); // Invoke parent's close method
+    },
 });

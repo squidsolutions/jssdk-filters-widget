e085871f5fa8ad6d667536d7906bcbdb18c59a7e
Thomas Scott <thomas.scott@squidsolutions.com>
2015-10-28 16:50:27 +0100

    refactor date picker logic

--- dist/squid_api_filters-widget.js
+++ dist/squid_api_filters-widget.js
@@ -2302,20 +2302,24 @@ $.widget( "ui.dialog", $.ui.dialog, {
                 var endDate = moment.utc(picker.endDate).utc().toDate();
                 me.updateFacet(facet, startDate, endDate);
             });
-
+                       
             this.$el.find("span").on('change.daterangepickerLeft', function(ev, calendar) {
             	if ($(calendar).hasClass("left")) {
             		$('.daterangepicker').find('.left td.available:not(.off):first').trigger('click');
-            	} else {
+            	} else if ($(calendar).hasClass("right")) {
             		$('.daterangepicker').find('.right td.available:not(.off):last').trigger('click');
+            	} else {
+            		$('.daterangepicker').find('.left td.available:not(.off):first').trigger('click');
             	}
             });
 
             this.$el.find("span").on('change.daterangepickerRight', function(ev, calendar) {
             	if ($(calendar).hasClass("left")) {
-            		$('.daterangepicker').find('.left td.available:not(.off):last').trigger('click');
-            	} else {
+            		$('.daterangepicker').find('.left td.available:not(.off):first').trigger('click');
+            	} else if ($(calendar).hasClass("right")) {
             		$('.daterangepicker').find('.right td.available:not(.off):last').trigger('click');
+            	} else {
+            		$('.daterangepicker').find('.left td.available:not(.off):first').trigger('click');
             	}
             });
         }
